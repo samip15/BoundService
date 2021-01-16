55d4e52e63f16cbe5df09b46f7fa751746b8d594
Samip <samipgnyawali15@gmail.com>
2021-01-16 17:01:23 +0545

    first commit

---
+++ app/src/main/java/com/example/boundservicewithmvvm/MainActivityViewModel.java
@@ -0,0 +1,55 @@
+package com.example.boundservicewithmvvm;
+
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    private static final String TAG = "MainActivityViewModel";
+
+    private MutableLiveData<Boolean> mIsProgressBarUpdating = new MutableLiveData<>();
+    private MutableLiveData<MyService.MyBinder> mBinder = new MutableLiveData<>();
+
+
+    // Keeping this in here because it doesn't require a context
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder iBinder) {
+            Log.d(TAG, "ServiceConnection: connected to service.");
+            // We've bound to MyService, cast the IBinder and get MyBinder instance
+            MyService.MyBinder binder = (MyService.MyBinder) iBinder;
+            mBinder.postValue(binder);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            Log.d(TAG, "ServiceConnection: disconnected from service.");
+            mBinder.postValue(null);
+        }
+    };
+
+
+    public ServiceConnection getServiceConnection() {
+        return serviceConnection;
+    }
+
+    public LiveData<MyService.MyBinder> getBinder() {
+        return mBinder;
+    }
+
+
+    public LiveData<Boolean> getIsProgressBarUpdating() {
+        return mIsProgressBarUpdating;
+    }
+
+    public void setIsProgressBarUpdating(boolean isUpdating) {
+        mIsProgressBarUpdating.postValue(isUpdating);
+    }
+
+
+}
